fa1181686fb7479512fbbf58e07a07d7b0472078
fkh75 <104203785+fkh75@users.noreply.github.com>
2023-06-23 14:32:31 +0300

    Sdded New Files

---
+++ script.js
@@ -0,0 +1,141 @@
+var mobileMenuCloseButton = document.getElementById("mobile-menu-close-button")
+var mobileMenuContainer = document.getElementById("mobile-menu-container")
+var burgerButton = document.getElementById("burger-button")
+var horizontalScroller = document.getElementById("horizontal-scroller")
+var horizontaContainer = document.getElementById("horizontal-scroll-container")
+
+
+
+
+// NAV MENU
+mobileMenuCloseButton.addEventListener('click', () => {
+    mobileMenuContainer.style.display = "none"
+})
+
+burgerButton.addEventListener('click', () => {
+    mobileMenuContainer.style.display = "block"
+})
+
+
+// Gallery
+var masonry
+$(document).ready(() => {
+        masonry = new MiniMasonry({
+        container: '.gallery-container',
+        surroundingGutter: false,
+    });
+})
+
+
+
+function ChangeCategory(category){
+    var galleryItems = $('.gallery-item')
+    galleryItems.each((index, elem) => {
+
+        if(category.toLowerCase() == 'all'){
+            $(elem).show()
+            masonry.layout()
+            return
+        }
+    
+        if($(elem).data("type") == undefined) {
+            console.log("It is undefined")
+            $(elem).hide()
+            masonry.layout()
+            return
+        }
+        else {
+            $(elem).show() 
+        }
+        if($(elem).data("type").toLowerCase() != category.toLowerCase()){
+            $(elem).hide()
+        }
+        else {
+            $(elem).show() 
+        }
+
+        masonry.layout()
+    });
+}
+  
+//   var masonry2 = new MiniMasonry({
+//     container: document.querySelector('.container_2'),
+//     surroundingGutter: false,
+//     gutterX: 40,
+//     gutterY: 10
+//   });
+
+
+var dragValue = 0;
+var isDragging = false
+var lastDragValue = 0;
+
+
+
+window.addEventListener("mousemove", (event) => {
+
+    if(isDragging == false) return
+
+
+    var delta = lastDragValue - event.screenX
+    console.log(delta)
+
+    if(lastDragValue > event.screenX){
+        dragValue = -delta
+    }
+    else {
+        dragValue = -delta
+    }
+
+    var scrollerWidth = horizontalScroller.clientWidth
+
+    var offsets = horizontalScroller.getBoundingClientRect();
+    var left = offsets.left
+
+    if(left < -(scrollerWidth / 2)) 
+    {
+        left = -(scrollerWidth / 2)
+    }
+    else if(left > scrollerWidth / 2)
+    {
+        left = scrollerWidth / 2
+    }
+
+    horizontalScroller.style.left = left + dragValue + "px"
+
+    lastDragValue = event.screenX
+
+})
+
+window.addEventListener("mousedown", (event) => {
+    if(horizontalScroller.contains(event.target)){
+        isDragging = true
+        dragValue = 0
+        lastDragValue = event.screenX
+    }
+})
+
+window.addEventListener("mouseup", (event) => {
+    isDragging = false
+})
+
+
+$('.gallery-container').click(function(){
+    $('.gallery-container').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-type');
+    $('.gallery-item').isotope({
+        filter:selector
+    });
+    return  false;
+});
+$(document).ready(function() {
+var popup_btn = $('.popup-btn');
+popup_btn.magnificPopup({
+type : 'image',
+gallery : {
+    enabled : true
+}
+});
+});
